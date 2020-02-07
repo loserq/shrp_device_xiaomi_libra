5b5b73e2683c37f44196641cd96f13a8cfb318d8
Dmitry Gomzyakov <nicerok11@gmail.com>
2020-02-07 13:25:36 +0500

    libra: init

---
+++ BoardConfig.mk
@@ -0,0 +1,107 @@
+#
+# Copyright (C) 2017 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Platform
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_SUFFIX := _64
+
+# Arch
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53.a57
+
+TARGET_USES_64_BIT_BINDER := true
+TARGET_CPU_SMP := true
+
+# Kernel
+TARGET_KERNEL_CONFIG := libra_defconfig
+TARGET_KERNEL_SOURCE := /home/kyoto44/havoc/kernel/xiaomi/libra
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET := 0x02000000
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := /home/kyoto44/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5 androidboot.selinux=permissive
+
+
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
+TARGET_BOARD_PLATFORM := msm8992
+TARGET_BOOTLOADER_BOARD_NAME := msm8992
+
+#TARGET_PREBUILT_KERNEL := device/xiaomi/libra/prebuilt/kernel
+
+
+TARGET_USES_LOGD := true
+TWRP_INCLUDE_LOGCAT := true
+
+# Keymaster
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 67108864
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 402653184
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 2013265920
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12469648896
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_USES_ION := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+DEVICE_RESOLUTION := 1080x1920
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_RECOVERY_FSTAB = device/xiaomi/libra/recovery.fstab
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+
+# supress error messages while building
+ALLOW_MISSING_DEPENDENCIES := true
+
+SHRP_PATH := device/xiaomi/libra
+# Maintainer name
+SHRP_MAINTAINER := kyoto44
+# Device codename
+SHRP_DEVICE_CODE := libra
+# put this 0 if device has no EDL mode
+SHRP_EDL_MODE := 1
+SHRP_EXTERNAL := /usb-otg
+SHRP_INTERNAL := /sdcard
+#SHRP_OTG := /usb-otg
+# Put 0 to disable flashlight
+SHRP_FLASH := 1
+# These are led paths, find yours then put here
+SHRP_FONP_1 := /sys/class/leds/led:torch_0/brightness
+SHRP_FONP_2 := /sys/class/leds/led:torch_1/brightness
+SHRP_FONP_3 := /sys/class/leds/led:switch/brightness
+# Max Brightness of LED
+SHRP_FLASH_MAX_BRIGHTNESS := 200
+# Check your device's recovery path, dont use blindly
+SHRP_REC := /dev/block/bootdevice/by-name/recovery
